fc4cd4a0df3a00284b7c0bcfd952dfa9d4b19cfd
pramodmorthala13 <79184012+pramodmorthala13@users.noreply.github.com>
2022-05-03 11:08:06 +0530

    posthog1
    posthog1

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,45 +41,17 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-cookiehub-banner`,
+      resolve: `gatsby-plugin-posthog`,
       options: {
-        // The ID is part of the CookieHub URL: https://cookiehub.net/cc/YOUR_COOKIEHUB_ID.js
-        cookieHubId: "YOUR_COOKIEHUB_BANNER_ID",
-        // Optional parameter (default false) - Use new v2 API.
-        cookieHubV2Api: false,
-        // Categories configured with CookieHub
-        categories: [
-          {
-            categoryName: "analytics", // Unique id of the category which is set by Cookiehub.
-            cookieName: "gatsby-plugin-google-analytics-gdpr_cookies-enabled", // Your custom cookie name
-          },
-          {
-            categoryName: "marketing",
-            cookieName: "marketing-enabled",
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-XHZFTDE6QH", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-        },
+        // Specify the API key for your Posthog Project (required)
+        apiKey: "phc_RTmH4J8QRMDOrvF05HFOufKwU004g0QQE2jezSDQr8H",
+        // Specify the app host if self-hosting (optional, default: https://app.posthog.com)
+        apiHost: "https://gatsbypromain.gatsbyjs.io/",
+        // Puts tracking script in the head instead of the body (optional, default: true)
+        head: true,
+        // Enable posthog analytics tracking during development (optional, default: false)
+        isEnabledDevMode: true
+        // Pass custom variables to posthog.init() (optional)
       },
     },
   ],
